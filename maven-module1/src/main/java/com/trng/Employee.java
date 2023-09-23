8c49db6fe122b2fbc4a95c2f6465d6ef545cc34b
subodhsharmabgp <subodhsharma.bgp@gmail.com>
2023-09-23 14:13:30 +0530

    This is initial project commit by @xyz

---
+++ maven-module1/src/main/java/com/trng/Employee.java
@@ -0,0 +1,11 @@
+package com.trng;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Employee {
+    private String empName;
+}
+
---
+++ maven-module2/src/main/java/com/trng/EmployeeDemo.java
@@ -0,0 +1,9 @@
+package com.trng;
+
+public class EmployeeDemo {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setEmpName("Ram");
+        System.out.println(employee.getEmpName());
+    }
+}
